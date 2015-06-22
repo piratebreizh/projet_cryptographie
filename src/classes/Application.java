729787ed19200b775ec5523dccd8932a3641e066
samuelfr <samuel.frigout@gmail.com>
2015-06-22 17:34:10 +0200

    Add encode function

--- src/classes/Application.java
+++ src/classes/Application.java
@@ -1,21 +1,27 @@
 package classes;
 
+import interfaces.ICipher;
+
 import java.io.File;
 
 public class Application {
+
+	public static String BASE_PATH = "D:\\ESGI\\Cryptographie\\Substitution_monoalpha\\test\\";
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Cipher c = new Cipher();
-		File key = new File("key.txt");
-		File message = new File("message.txt");
-		File encoded = new File("encoded.txt");
-		File decoded = new File("decoded.txt");
-		c.generateKey(key);
-		//c.encode(message, key, encoded);
-		//c.decode(encoded, key, decoded);
+		ICipher cipher = new Cipher();
+		File maCle = new File(BASE_PATH+"maCLe.txt");
+		cipher.generateKey(maCle);
+
+		File message = new File(BASE_PATH+"message.txt");
+		File encoded = new File(BASE_PATH+"encoded.txt");
+		File decoded = new File(BASE_PATH+"decoded.txt");
+
+		cipher.encode(message, maCle, encoded);
+		cipher.decode(encoded, maCle, decoded);
 
 	}
 
--- src/classes/Cipher.java
+++ src/classes/Cipher.java
@@ -1,9 +1,6 @@
 package classes;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
@@ -12,72 +9,93 @@ import interfaces.ICipher;
 
 public class Cipher implements ICipher {
 
-	public void encode(File message, File key, File encoded) {
-		
-		
-	}
+    public void encode(File message, File key, File encoded) {
 
-	public void decode(File encoded, File key, File decoded) {
-		
-		
-	}
+        try {
+            //Message
+            InputStream inputStream = new FileInputStream(message);
+            InputStreamReader input = new InputStreamReader(inputStream);
+            BufferedReader br = new BufferedReader(input);
+            //Encoded
+            OutputStreamWriter outputStreamWriter = new FileWriter(encoded);
+            //Lecture
+            int intChar;
+            while ((intChar = br.read()) != -1) {
+                char ch = (char) intChar;
+                char chEncoded = getEncodeChar(ch, key);
+                //Encodage
+                outputStreamWriter.write(chEncoded);
+            }
+            br.close();
+            outputStreamWriter.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void generateKey(File key) {
+    public void decode(File encoded, File key, File decoded) {
 
-		ArrayList<Character> listkey = new ArrayList<Character>();
-		listkey.add('A');
-		listkey.add('B');
-		listkey.add('C');
-		listkey.add('D');
-		listkey.add('E');
-		listkey.add('F');
-		listkey.add('G');
-		listkey.add('H');
-		listkey.add('I');
-		listkey.add('J');
-		listkey.add('K');
-		listkey.add('L');
-		listkey.add('M');
-		listkey.add('N');
-		listkey.add('O');
-		listkey.add('P');
-		listkey.add('Q');
-		listkey.add('R');
-		listkey.add('S');
-		listkey.add('T');
-		listkey.add('U');
-		listkey.add('V');
-		listkey.add('W');
-		listkey.add('X');
-		listkey.add('Y');
-		listkey.add('Z');
-		listkey.add(' ');
-		listkey.add('.');
-		listkey.add(',');
-		listkey.add(';');
-		listkey.add(':');
-		listkey.add('"');
-		listkey.add('\'');
-		
-		long seed = System.nanoTime();
-		Collections.shuffle(listkey,new Random(seed));
-		
-		PrintWriter pw;
-		try {
-			pw = new PrintWriter(new FileOutputStream(key));
-	    for (Character c : listkey)
-	        pw.print(c);
-	    pw.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public static Character getEncodeChar(Character c, File key){
-		
-		char ca = 'a';
-		return ca;
-	}
+
+    }
+
+    public void generateKey(File key) {
+
+        ArrayList<Character> listkey = new ArrayList<Character>();
+        listkey.add('A');
+        listkey.add('B');
+        listkey.add('C');
+        listkey.add('D');
+        listkey.add('E');
+        listkey.add('F');
+        listkey.add('G');
+        listkey.add('H');
+        listkey.add('I');
+        listkey.add('J');
+        listkey.add('K');
+        listkey.add('L');
+        listkey.add('M');
+        listkey.add('N');
+        listkey.add('O');
+        listkey.add('P');
+        listkey.add('Q');
+        listkey.add('R');
+        listkey.add('S');
+        listkey.add('T');
+        listkey.add('U');
+        listkey.add('V');
+        listkey.add('W');
+        listkey.add('X');
+        listkey.add('Y');
+        listkey.add('Z');
+        listkey.add(' ');
+        listkey.add('.');
+        listkey.add(',');
+        listkey.add(';');
+        listkey.add(':');
+        listkey.add('"');
+        listkey.add('\'');
+
+        long seed = System.nanoTime();
+        Collections.shuffle(listkey, new Random(seed));
+
+        PrintWriter pw;
+        try {
+            pw = new PrintWriter(new FileOutputStream(key));
+            for (Character c : listkey)
+                pw.print(c);
+            pw.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static char getEncodeChar(char c, File key) {
+
+        char ca = 'a';
+        return ca;
+    }
 
 }
