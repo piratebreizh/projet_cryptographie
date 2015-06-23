c28f9ea17a294a0a15361c0a12196ee3865bbaca
piratebreizh <alexandre.d.bzh@gmail.com>
2015-06-23 15:47:46 +0200

    debut bruteForce

---
+++ src/attaques/BruteForce.java
@@ -0,0 +1,18 @@
+package attaques;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class BruteForce {
+
+	
+	private ArrayList<String> listeMotsConfirmation;
+	
+	BruteForce(){
+		
+	}
+	
+	private void runAttaque(File _message, File _enCoded){
+		
+	}
+}
