722aef83d13d66ebf78e98b1c54617dc6af5d983
samuelfr <samuel.frigout@gmail.com>
2015-06-23 15:47:09 +0200

    update CesarCipher

--- src/classes/Application.java
+++ src/classes/Application.java
@@ -13,29 +13,49 @@ public class Application {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		ICipher cipher = new MonoCipher();
-		File maCle = new File(BASE_PATH+"key.txt");
-//		cipher.generateKey(maCle);
-
-		File message = new File(BASE_PATH+"message.txt");
-		File source = new File(BASE_PATH+"source.txt");
-		File encoded = new File(BASE_PATH+"encoded.txt");
-		File decoded = new File(BASE_PATH+"decoded.txt");
-
-//		cipher.encode(message, maCle, encoded);
-//		cipher.decode(encoded, maCle, decoded);
-
-
-        File sourceFrequence = new File(BASE_PATH+"texte_reference.txt");
-
-		File foundKey = new File(BASE_PATH+"foundKey.txt");
-        File decodedAttack = new File(BASE_PATH+"decodedAttack.txt");
-
-		MonoEncodedAttack attack = new MonoEncodedAttack();
- attack.createFrequence(sourceFrequence);
-		attack.findkey(encoded, foundKey);
-
-        cipher.decode(encoded,foundKey,decodedAttack);
+        encodeCesar();
 	}
 
+    public static void encodeCesar(){
+        ICipher cipher = new CesarCipher();
+        File maCle = new File(BASE_PATH+"keyCesar.txt");
+
+        File message = new File(BASE_PATH+"messageCesar.txt");
+        File encoded = new File(BASE_PATH+"encodedCesar.txt");
+        File decoded = new File(BASE_PATH+"decodedCesar.txt");
+
+        cipher.encode(message, maCle, encoded);
+        cipher.decode(encoded, maCle, decoded);
+    }
+
+    public static void encodeMono(){
+        ICipher cipher = new MonoCipher();
+        File maCle = new File(BASE_PATH+"key.txt");
+		cipher.generateKey(maCle);
+
+        File message = new File(BASE_PATH+"message.txt");
+        File source = new File(BASE_PATH+"source.txt");
+        File encoded = new File(BASE_PATH+"encoded.txt");
+        File decoded = new File(BASE_PATH+"decoded.txt");
+
+		cipher.encode(message, maCle, encoded);
+		cipher.decode(encoded, maCle, decoded);
+    }
+
+
+    public static void launchAttack(){
+        ICipher cipher = new MonoCipher();
+
+        File encoded = new File(BASE_PATH+"encoded.txt");
+
+        File sourceFrequence = new File(BASE_PATH+"texte_reference.txt");
+        File foundKey = new File(BASE_PATH+"foundKey.txt");
+        File decodedAttack = new File(BASE_PATH+"decodedAttack.txt");
+
+        MonoEncodedAttack attack = new MonoEncodedAttack();
+        attack.createFrequence(sourceFrequence);
+        attack.findkey(encoded, foundKey);
+
+        cipher.decode(encoded,foundKey,decodedAttack);
+    }
 }
---
+++ src/classes/CesarCipher.java
@@ -0,0 +1,114 @@
+package classes;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Map.Entry;
+
+import interfaces.ICipher;
+
+public class CesarCipher implements ICipher {
+
+    private char codeCesar;
+
+    private static final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + " .,;:\"'";
+
+    @Override
+    public void encode(File message, File key, File encoded) {
+        try {
+            //Message
+            InputStream inputStream = new FileInputStream(message);
+            InputStreamReader input = new InputStreamReader(inputStream);
+            BufferedReader br = new BufferedReader(input);
+            //Encoded
+            OutputStreamWriter outputStreamWriter = new FileWriter(encoded);
+            //Lecture
+            int intChar;
+            while ((intChar = br.read()) != -1) {
+                char ch = (char) intChar;
+                char chEncoded = getEncodeChar(ch);
+                //Encodage
+                outputStreamWriter.write(chEncoded);
+            }
+            br.close();
+            outputStreamWriter.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void decode(File encoded, File key, File decoded) {
+        try {
+
+            //Encoded
+            InputStream inputStream = new FileInputStream(encoded);
+            InputStreamReader input = new InputStreamReader(inputStream);
+            BufferedReader br = new BufferedReader(input);
+            //Encoded
+            OutputStreamWriter outputStreamWriter = new FileWriter(decoded);
+            //Lecture
+            int intChar;
+            while ((intChar = br.read()) != -1) {
+                char ch = (char) intChar;
+                //Decodage
+                char chEncoded = getDecodeChar(ch);
+                outputStreamWriter.write(chEncoded);
+            }
+            br.close();
+            outputStreamWriter.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void generateKey(File key) {
+        try {
+            //Message
+            InputStream inputStream = new FileInputStream(key);
+            InputStreamReader input = new InputStreamReader(inputStream);
+            BufferedReader br = new BufferedReader(input);
+            //Lecture
+            int intChar;
+            while ((intChar = br.read()) != -1) {
+                codeCesar = (char) intChar;
+            }
+            br.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public char getEncodeChar(char c) {
+        int posCesar = alphabet.indexOf(codeCesar);
+        int posC = alphabet.indexOf(c);
+
+        return alphabet.charAt((posCesar+posC)%alphabet.length());
+    }
+
+    public char getDecodeChar(char c) {
+        int posCesar = alphabet.indexOf(codeCesar);
+        int posC = alphabet.indexOf(c);
+
+        int pos = (posC-posCesar)%alphabet.length();
+        return alphabet.charAt(pos);
+    }
+
+}
