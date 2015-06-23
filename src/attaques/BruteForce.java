1ab556ddbddf08f6887e227b168dd0cb3e786635
piratebreizh <alexandre.d.bzh@gmail.com>
2015-06-23 15:52:48 +0200

    no message

--- src/attaques/BruteForce.java
+++ src/attaques/BruteForce.java
@@ -9,10 +9,22 @@ public class BruteForce {
 	private ArrayList<String> listeMotsConfirmation;
 	
 	BruteForce(){
-		
+		listeMotsConfirmation = new ArrayList<String>();
+		listeMotsConfirmation.add("EST");
+		listeMotsConfirmation.add("LES");
+		listeMotsConfirmation.add("NE");
+		listeMotsConfirmation.add("EN");
+		listeMotsConfirmation.add("AU");
+		listeMotsConfirmation.add("AVEC");
+		listeMotsConfirmation.add("LORS");
+		listeMotsConfirmation.add("LE");
+		listeMotsConfirmation.add("LA");
+		listeMotsConfirmation.add("IL");
+		listeMotsConfirmation.add("JE");
 	}
 	
 	private void runAttaque(File _message, File _enCoded){
+		
 		
 	}
 }
