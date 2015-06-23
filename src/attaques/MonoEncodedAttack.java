d7d8d3a7ad576927df3d9e464319235102ab4469
antoineludwig <antoine.ludwig60@gmail.com>
2015-06-23 10:37:42 +0200

    frequence

--- src/attaques/MonoEncodedAttack.java
+++ src/attaques/MonoEncodedAttack.java
@@ -1,14 +1,17 @@
 package attaques;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class MonoEncodedAttack {
-
-	private Map<Character, Float> frequence;
+	private ArrayList<Character> frequence;
 	
 	public MonoEncodedAttack(){
-		frequence.put('A', 0.0);
+		frequence = new ArrayList<Character>();
+		frequence.add('A');
+		
 	}
 	
 	public void findkey(File encoded, File foundedKey){
