a4fc0fbca288c1155e7a9977e4af0c2a671b0706
antoineludwig <antoine.ludwig60@gmail.com>
2015-06-23 10:56:14 +0200

    ajout liste frequence

--- src/attaques/MonoEncodedAttack.java
+++ src/attaques/MonoEncodedAttack.java
@@ -2,16 +2,96 @@ package attaques;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MonoEncodedAttack {
 	private ArrayList<Character> frequence;
 	
 	public MonoEncodedAttack(){
 		frequence = new ArrayList<Character>();
+		frequence.add('E');
 		frequence.add('A');
-		
+		frequence.add('I');
+		frequence.add('S');
+		frequence.add('N');
+		frequence.add('R');
+		frequence.add('T');
+		frequence.add('O');
+		frequence.add('L');
+		frequence.add('U');
+		frequence.add('D');
+		frequence.add('C');
+		frequence.add('M');
+		frequence.add('P');
+		frequence.add('É');
+		frequence.add(' ');
+		frequence.add('G');
+		frequence.add('B');
+		frequence.add('V');
+		frequence.add('H');
+		frequence.add('F');
+		frequence.add(',');
+		frequence.add('1');
+		frequence.add('.');
+		frequence.add('\'');
+		frequence.add('0');
+		frequence.add('9');
+		frequence.add('Q');
+		frequence.add('-');
+		frequence.add('2');
+		frequence.add('Y');
+		frequence.add('8');
+		frequence.add(')');
+		frequence.add('(');
+		frequence.add('X');
+		frequence.add('3');
+		frequence.add('5');
+		frequence.add('6');
+		frequence.add('4');
+		frequence.add('J');
+		frequence.add('7');
+		frequence.add(':');
+		frequence.add('È');
+		frequence.add('À');
+		frequence.add('K');
+		frequence.add('?');
+		frequence.add('W');
+		frequence.add('Z');
+		frequence.add('Ê');
+		frequence.add('"');
+		frequence.add('/');
+		frequence.add('Ç');
+		frequence.add('>');
+		frequence.add('#');
+		frequence.add('<');
+		frequence.add('·');
+		frequence.add(';');
+		frequence.add('Ô');
+		frequence.add('«');
+		frequence.add('»');
+		frequence.add('Â');
+		frequence.add('Î');
+		frequence.add(']');
+		frequence.add('{');
+		frequence.add('[');
+		frequence.add('}');
+		frequence.add('°');
+		frequence.add('Û');
+		frequence.add('Ù');
+		frequence.add('Ï');
+		frequence.add('=');
+		frequence.add('%');
+		frequence.add('+');
+		frequence.add('!');
+		frequence.add('_');
+		frequence.add('Á');
+		frequence.add('&');
+		frequence.add('Ü');
+		frequence.add('²');
+		frequence.add('*');
+		frequence.add('Ë');
+		frequence.add('Ö');
+		frequence.add('Í');
+
 	}
 	
 	public void findkey(File encoded, File foundedKey){
