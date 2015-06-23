aef9a0da9682c2aafaea6f3f61abe5513382e47f
piratebreizh <alexandre.d.bzh@gmail.com>
2015-06-23 16:52:41 +0200

    eaz

--- src/classes/CesarCipher.java
+++ src/classes/CesarCipher.java
@@ -16,110 +16,117 @@ import interfaces.ICipher;
 
 public class CesarCipher implements ICipher {
 
-    private char codeCesar;
+	private char codeCesar;
 
-    private static final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + " .,;:\"'";
+	private static final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + " .,;:\"'";
 
-    @Override
-    public void encode(File message, File key, File encoded) {
-        try {
-            //Message
-            InputStream inputStream = new FileInputStream(message);
-            InputStreamReader input = new InputStreamReader(inputStream);
-            BufferedReader br = new BufferedReader(input);
-            //Encoded
-            OutputStreamWriter outputStreamWriter = new FileWriter(encoded);
-            //Lecture
-            int intChar;
-            while ((intChar = br.read()) != -1) {
-                char ch = (char) intChar;
-                char chEncoded = getEncodeChar(ch);
-                //Encodage
-                outputStreamWriter.write(chEncoded);
-            }
-            br.close();
-            outputStreamWriter.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+	@Override
+	public void encode(File message, File key, File encoded) {
+		try {
+			//Message
+			InputStream inputStream = new FileInputStream(message);
+			InputStreamReader input = new InputStreamReader(inputStream);
+			BufferedReader br = new BufferedReader(input);
+			//Encoded
+			OutputStreamWriter outputStreamWriter = new FileWriter(encoded);
+			//Lecture
+			int intChar;
+			while ((intChar = br.read()) != -1) {
+				char ch = (char) intChar;
+				char chEncoded = getEncodeChar(ch);
+				//Encodage
+				outputStreamWriter.write(chEncoded);
+			}
+			br.close();
+			outputStreamWriter.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-    }
+	}
 
-    @Override
-    public void decode(File encoded, File key, File decoded) {
-        try {
-            //Encoded
-            InputStream inputStream = new FileInputStream(encoded);
-            InputStreamReader input = new InputStreamReader(inputStream);
-            BufferedReader br = new BufferedReader(input);
-            //Encoded
-            OutputStreamWriter outputStreamWriter = new FileWriter(decoded);
-            //Lecture
-            int intChar;
-            while ((intChar = br.read()) != -1) {
-                char ch = (char) intChar;
-                //Decodage
-                char chEncoded = getDecodeChar(ch);
-                outputStreamWriter.write(chEncoded);
-            }
-            br.close();
-            outputStreamWriter.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+	@Override
+	public void decode(File encoded, File key, File decoded) {
+		try {
+			//Encoded
+			InputStream inputStream = new FileInputStream(encoded);
+			InputStreamReader input = new InputStreamReader(inputStream);
+			BufferedReader br = new BufferedReader(input);
+			//Encoded
+			OutputStreamWriter outputStreamWriter = new FileWriter(decoded);
+			//Lecture
+			int intChar;
+			while ((intChar = br.read()) != -1) {
+				char ch = (char) intChar;
+				//Decodage
+				char chEncoded = getDecodeChar(ch);
+				outputStreamWriter.write(chEncoded);
+			}
+			br.close();
+			outputStreamWriter.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-    }
+	}
 
-    public String decodeString(String encoded, Character key)
-    {
-        String decoded = "";
-        for(int i=0; i<encoded.length() ; i++){
-            //Decodage
-            char chDecode = getDecodeChar(encoded.charAt(i));
-            decoded += chDecode;
-        }
+	public String decodeString(String encoded, Character key)
+	{
+		String decoded = "";
+		for(int i=0; i<encoded.length() ; i++){
+			//Decodage
+			char chDecode = getDecodeChar(encoded.charAt(i));
+			decoded += chDecode;
+		}
 
-        return decoded;
-    }
+		return decoded;
+	}
 
-    @Override
-    public void generateKey(File key) {
-        try {
-            //Message
-            InputStream inputStream = new FileInputStream(key);
-            InputStreamReader input = new InputStreamReader(inputStream);
-            BufferedReader br = new BufferedReader(input);
-            //Lecture
-            int intChar;
-            while ((intChar = br.read()) != -1) {
-                codeCesar = (char) intChar;
-            }
-            br.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+	@Override
+	public void generateKey(File key) {
+		try {
+			//Message
+			InputStream inputStream = new FileInputStream(key);
+			InputStreamReader input = new InputStreamReader(inputStream);
+			BufferedReader br = new BufferedReader(input);
+			//Lecture
+			int intChar;
+			while ((intChar = br.read()) != -1) {
+				codeCesar = (char) intChar;
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-    }
+	}
 
-    public char getEncodeChar(char c) {
-        int posCesar = alphabet.indexOf(codeCesar);
-        int posC = alphabet.indexOf(c);
+	public char getEncodeChar(char c) {
+		int posCesar = alphabet.indexOf(codeCesar);
+		int posC = alphabet.indexOf(c);
 
-        return alphabet.charAt((posCesar+posC)%alphabet.length());
-    }
+		if(posC>=0){
+			return alphabet.charAt((posCesar+posC)%alphabet.length());
+		}else{
+			return c;
+		}
+	}
 
-    public char getDecodeChar(char c) {
-        int posCesar = alphabet.indexOf(codeCesar);
-        int posC = alphabet.indexOf(c);
-
-        int pos = (posC-posCesar)%alphabet.length();
-        return alphabet.charAt(pos);
-    }
+	public char getDecodeChar(char c) {
+		int posCesar = alphabet.indexOf(codeCesar);
+		int posC = alphabet.indexOf(c);
+		int pos = (posC-posCesar)%alphabet.length();
+		if(pos>=0){
+			return alphabet.charAt(pos);
+		}else{
+			return c;
+		}
+	}
 
 }
