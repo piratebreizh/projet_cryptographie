88ceecf2aa98e21ef7cec1271521c3c7c5d434cd
samuelfr <samuel.frigout@gmail.com>
2015-06-26 09:31:21 +0200

    update TranspositionCipher

--- src/classes/TranspositionCipher.java
+++ src/classes/TranspositionCipher.java
@@ -3,16 +3,9 @@ package classes;
 import interfaces.ICipher;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class TranspositionCipher implements ICipher {
     private ArrayList<Integer> keyList;
@@ -20,7 +13,7 @@ public class TranspositionCipher implements ICipher {
 
     @Override
     public void encode(File message, File key, File encoded) {
-        generateKey(key);
+        readKey(key);
         try {
             FileReader reader = new FileReader(message);
             FileWriter writer = new FileWriter(encoded);
@@ -28,16 +21,15 @@ public class TranspositionCipher implements ICipher {
             //Lecture
             int intChar;
             while ((intChar = reader.read()) != -1) {
-                if(block.length() != keyList.size()){
-                    block.append((char) intChar);
-                }else{
+                if (block.length() == keyList.size()) {
                     writer.write(getEncodedBlock(block.toString()));
-                    block.delete(0,block.length());
+                    block.delete(0, block.length());
                 }
+                block.append((char) intChar);
             }
             //Cas où le dernier block n'est pas plein
-            if(block.length() > 0){
-                while (block.length() != keyList.size()){
+            if (block.length() > 0) {
+                while (block.length() != keyList.size()) {
                     block.append(' ');
                 }
                 writer.write(getEncodedBlock(block.toString()));
@@ -50,57 +42,72 @@ public class TranspositionCipher implements ICipher {
         }
     }
 
+    @Override
+    public void decode(File encoded, File key, File decoded) {
+        readKey(key);
+        try {
+            //Encoded
+            FileReader reader = new FileReader(encoded);
+            //decoded
+            FileWriter writer = new FileWriter(decoded);
+            //Lecture
+            int intChar;
+            StringBuilder block = new StringBuilder();
+
+            while ((intChar = reader.read()) != -1) {
+                if (block.length() == keyList.size()) {
+                    String decodedBlock = getDecodedBlock(block.toString());
+                    writer.write(decodedBlock);
+                    block.delete(0, block.length());
+                }
+                block.append((char) intChar);
+            }
+            if(block.length() != 0){
+                String decodedBlock = getDecodedBlock(block.toString());
+                writer.write(decodedBlock);
+            }
+            reader.close();
+            writer.flush();
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private String getEncodedBlock(String block) {
         StringBuilder builder = new StringBuilder();
-        for(int i=0 ; i<keyList.size() ; i++){
+        for (int i = 0; i < keyList.size(); i++) {
             builder.append(block.charAt(keyList.get(i)));
         }
 
         return builder.toString();
     }
 
-    @Override
-    public void decode(File encoded, File key, File decoded) {
-        try {
-            //Encoded
-            InputStream inputStream = new FileInputStream(encoded);
-            InputStreamReader input = new InputStreamReader(inputStream);
-            BufferedReader br = new BufferedReader(input);
-            //Encoded
-            OutputStreamWriter outputStreamWriter = new FileWriter(decoded);
-            //Lecture
-            int intChar;
-            StringBuilder block = null;
-            
-            while ((intChar = br.read()) != -1) {
-                if(block.length()<BLOCK_LENGTH){
-                	block.append(intChar);
-                }else{
-                	String decodedBlock = getDecodedBlock(block);
-                    outputStreamWriter.write(decodedBlock);
-                    block.delete(0, block.length());
-                }
-            }
-            br.close();
-            outputStreamWriter.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+    private String getDecodedBlock(String codedString) {
+        StringBuilder builder = new StringBuilder();
+        for(int i=0 ; i<keyList.size() ; i++){
+            int index = keyList.indexOf(i);
+            builder.append(codedString.charAt(index));
         }
+
+        return builder.toString();
     }
 
     @Override
     public void generateKey(File key) {
+    }
+
+    private void readKey(File key) {
         keyList = new ArrayList<>();
         int keyLength = 5;
-        for(int i=0 ; i< keyLength ; i++){
-            keyList.add(i);
-        }
-        Collections.shuffle(keyList);
-    }
-    
-    public String getDecodedBlock(StringBuilder codedString){
-    	return codedString.toString();
+//        for (int i = 0; i < keyLength; i++) {
+//            keyList.add(i);
+//        }
+//        Collections.shuffle(keyList);
+        keyList.add(2);
+        keyList.add(1);
+        keyList.add(0);
+        keyList.add(3);
+        keyList.add(4);
     }
 }
