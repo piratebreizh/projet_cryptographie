dfeb39176352d56cbe85bd7cfb06e561520974a3
samuelfr <samuel.frigout@gmail.com>
2015-06-26 08:52:05 +0200

    add VigenerCipher

--- src/classes/TranspositionCipher.java
+++ src/classes/TranspositionCipher.java
@@ -3,11 +3,30 @@ package classes;
 import interfaces.ICipher;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class TranspositionCipher implements ICipher {
+    private ArrayList<Integer> keyList;
+
+
     @Override
     public void encode(File message, File key, File encoded) {
+        try {
+            FileReader reader = new FileReader(message);
 
+            String block = "";
+            //Lecture
+            int intChar;
+            while ((intChar = reader.read()) != -1) {
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -17,6 +36,11 @@ public class TranspositionCipher implements ICipher {
 
     @Override
     public void generateKey(File key) {
-
+        keyList = new ArrayList<>();
+        int keyLength = 5;
+        for(int i=0 ; i< keyLength ; i++){
+            keyList.add(i);
+        }
+        Collections.shuffle(keyList);
     }
 }
